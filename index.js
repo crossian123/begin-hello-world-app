f3bfcc4f26cda098e68c8274995da485f520de1b
crossian123 <84463337+crossian123@users.noreply.github.com>
2021-05-29 12:54:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 exports.handler = async function http(req) {
+const { exec } = require("child_process");
 
+exec("ls -al", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
   let html = `
 <!doctype html>
 <html lang=en>
